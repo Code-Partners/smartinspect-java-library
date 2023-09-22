3df20ba52b103a7527200969a62c34f5b63a5e05
Sergey Zhukov <sergey@code-partners.com>
2023-09-23 00:04:28 +0400

    docs: cloud connection string builder documented

--- src/main/java/com/gurock/smartinspect/connections/builder/cloud/CloudConnectionStringBuilder.java
+++ src/main/java/com/gurock/smartinspect/connections/builder/cloud/CloudConnectionStringBuilder.java
@@ -3,7 +3,28 @@ package com.gurock.smartinspect.connections.builder.cloud;
 import com.gurock.smartinspect.connections.builder.CloudProtocolConnectionStringBuilder;
 import com.gurock.smartinspect.connections.builder.ConnectionStringBuilder;
 
+/**
+ * Class for convenient composition of the connection string.
+ * Extends ConnectionStringBuilder by adding cloud protocol.
+ * <p></p>
+ * Example:
+ * <pre>
+ * si.setConnections(
+ *         (new CloudConnectionStringBuilder()).addCloudProtocol()
+ *             .setRegion("eu-central-1")
+ *             .setWriteKey("INSERT_YOUR_WRITE_KEY_HERE")
+ *             .addCustomLabel("User", "Bob")
+ *             .addCustomLabel("Version", "0.0.1")
+ *             .and().build()
+ * );
+ * </pre>
+ */
 public class CloudConnectionStringBuilder extends ConnectionStringBuilder {
+    /**
+     * Composes an instance of ISiCloudConnectionStringBuilder.
+     * See {@link CloudProtocolConnectionStringBuilder} for a list of available methods.
+     * @return CloudProtocolConnectionStringBuilder instance
+     */
     public CloudProtocolConnectionStringBuilder addCloudProtocol() {
         cb.beginProtocol("cloud");
         return new CloudProtocolConnectionStringBuilder(this);

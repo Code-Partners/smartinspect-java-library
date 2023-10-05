4c1dc582e83de60478b53a710b6de59d13a4548a
Sergey Zhukov <sergey@code-partners.com>
2023-10-05 20:18:43 +0300

    docs: `BinaryContext` - XMLDoc converted to JavaDoc

--- src/main/java/com/gurock/smartinspect/BinaryContext.java
+++ src/main/java/com/gurock/smartinspect/BinaryContext.java
@@ -1,6 +1,6 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
@@ -9,68 +9,56 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-// <summary>
-//   This is the base class for all viewer contexts, which deal with
-//   binary data. A viewer context is the library-side representation
-//   of a viewer in the Console.
-// </summary>
-// <threadsafety>
-//   This class is not guaranteed to be threadsafe.
-// </threadsafety>
-
+/**
+ * This is the base class for all viewer contexts, which deal with
+ * binary data. A viewer context is the library-side representation
+ * of a viewer in the Console.
+ * <p>
+ * This class is not guaranteed to be threadsafe.
+ */
 public class BinaryContext extends ViewerContext
 {
 	private ByteArrayOutputStream fData;
 
-	// <summary>
-	//   Creates and initializes a BinaryContext instance.
-	// </summary>
-	// <param name="vi">The viewer ID to use.</param>
-
+	/**
+	 * Creates and initializes a BinaryContext instance.
+	 * @param vi The viewer ID to use.
+	 */
 	public BinaryContext(ViewerId vi)
 	{
 		super(vi);
 		this.fData = new ByteArrayOutputStream();		
 	}
 
-	// <summary>
-	//   Overridden. Returns the actual binary data which will be
-	//   displayed in the viewer specified by the getViewerId method.
-	// </summary>
-	// <returns>
-	//   The actual binary data which will be displayed in the viewer
-	//   specified by the getViewerId method.
-	// </returns>
-
+	/**
+	 * Overridden. Returns the actual binary data which will be
+	 * displayed in the viewer specified by the getViewerId
+	 *
+	 * @return The actual binary data which will be displayed in the viewer
+	 * specified by the getViewerId method.
+	 */
 	public byte[] getViewerData()
 	{
 		return this.fData.toByteArray();
 	}
 
-	// <summary>Resets the internal data stream.</summary>
-	// <remarks>
-	//   This method is intended to reset the internal data stream if
-	//   custom handling of data is needed by derived classes.
-	// </remarks>
-
+	/**
+	 * Resets the internal data stream.
+	 * <p>
+	 * This method is intended to reset the internal data stream if
+	 * custom handling of data is needed by derived classes.
+	 */
 	protected void resetData()
 	{
 		this.fData.reset();
 	}
 
-	// <summary>Loads the binary data from a file.</summary>
-	// <param name="fileName">
-	//   The name of the file to load the binary data from.
-	// </param>
-	// <exception>
-	// <table>
-	//   Exception Type         Condition
-	//   -                      -
-	//   NullPointerException   The fileName argument is null.
-	//   IOException            An I/O error occurred.
-	// </table>
-	// </exception>
-
+	/**
+	 * Loads the binary data from a file.
+	 * @param fileName The name of the file to load the binary data from.
+	 * @throws NullPointerException The fileName argument is null.
+	 * @throws IOException An I/O error occurred.
+	 */
 	public void loadFromFile(String fileName) throws IOException
 	{
 		if (fileName == null)
@@ -91,21 +79,12 @@ public class BinaryContext extends ViewerContext
 		}
 	}
 
-	// <summary>
-	//   Loads the binary data from a stream.
-	// </summary>
-	// <param name="is">
-	//   The stream to load the binary data from.
-	// </param>
-	// <exception>
-	// <table>
-	//   Exception Type         Condition
-	//   -                      -
-	//   NullPointerException   The is argument is null.
-	//   IOException            An I/O error occurred.
-	// </table>
-	// </exception>
-
+	/**
+	 * Loads the binary data from a stream.
+	 * @param is The stream to load the binary data from.
+	 * @throws NullPointerException The is argument is null.
+	 * @throws IOException An I/O error occurred.
+	 */
 	public void loadFromStream(InputStream is) throws IOException
 	{
 		if (is == null)
@@ -125,18 +104,11 @@ public class BinaryContext extends ViewerContext
 		}
 	}
 
-	// <summary>
-	//   Overloaded. Appends a buffer.
-	// </summary>
-	// <param name="b">The buffer to append.</param>
-	// <exception>
-	// <table>
-	//   Exception Type               Condition
-	//   -                            -
-	//   NullPointerException         The b argument is null.
-	// </table>
-	// </exception>
-
+	/**
+	 * Overloaded. Appends a buffer.
+	 * @param b The buffer to append.
+	 * @throws NullPointerException The b argument is null.
+	 */
 	public void appendBytes(byte[] b)
 	{
 		if (b == null)
@@ -149,28 +121,16 @@ public class BinaryContext extends ViewerContext
 		}
 	}
 
-	// <summary>
-	//   Overloaded. Appends a buffer. Lets you specify the offset in
-	//   the buffer and the amount of bytes to append.
-	// </summary>
-	// <param name="b">The buffer to append.</param>
-	// <param name="off">
-	//   The offset at which to begin appending.
-	// </param>
-	// <param name="len">The number of bytes to append.</param>
-	// <table>
-	//   Exception Type               Condition
-	//   -                            -
-	//   IndexOutOfBoundsException    The sum of the off and len
-	//                                  parameters is greater than the
-	//                                  actual buffer length or the off
-	//                                  or len arguments are negative.
-	//                                  
-	//   NullPointerException         The b argument is null.
-	//   
-	// </table>
-	// </exception>
-
+	/**
+	 * Overloaded. Appends a buffer. Lets you specify the offset in
+	 * the buffer and the amount of bytes to append.
+	 * @param b The buffer to append.
+	 * @param off The offset at which to begin appending.
+	 * @param len The number of bytes to append.
+	 * @throws IndexOutOfBoundsException The sum of the off and len parameters is greater than the
+	 * actual buffer length or the off or len arguments are negative.
+	 * @throws NullPointerException The b argument is null.
+	 */
 	public void appendBytes(byte[] b, int off, int len)
 	{
 		if (b == null)
@@ -183,10 +143,9 @@ public class BinaryContext extends ViewerContext
 		}
 	}
 
-	// <summary>
-	//   Overridden. Releases any resources used by this binary context.
-	// </summary>
-
+	/**
+	 * Overridden. Releases any resources used by this binary context.
+	 */
 	public void close()
 	{
 		if (this.fData != null)

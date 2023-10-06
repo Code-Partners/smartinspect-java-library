318ca02f5d861651b4be28338361792cd24b14af
Sergey Zhukov <sergey@code-partners.com>
2023-10-07 00:13:42 +0300

    docs: XMLDoc to Javadoc in 5 more files

--- src/main/java/com/gurock/smartinspect/DataViewerContext.java
+++ src/main/java/com/gurock/smartinspect/DataViewerContext.java
@@ -1,34 +1,26 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
-// <summary>
-//   Represents the data viewer in the Console which can display simple
-//   and unformatted text.
-// </summary>
-// <remarks>
-//   The data viewer in the Console interprets the <link LogEntry.getData,
-//   data of a Log Entry> as text and displays it in a read-only text
-//   field.
-//
-//   You can use the DataViewerContext class for creating custom log
-//   methods around <link Session.logCustomContext, logCustomContext> for
-//   sending custom text data.
-// </remarks>
-// <threadsafety>
-//   This class is not guaranteed to be threadsafe.
-// </threadsafety>
-
-public class DataViewerContext extends TextContext
-{
-	// <summary>
-	//   Creates and initializes a DataViewerContext instance.
-	// </summary>
-	
-	public DataViewerContext()
-	{
+/**
+ * Represents the data viewer in the Console which can display simple
+ * and unformatted text.
+ * <p>
+ * The data viewer in the Console interprets the data of a Log Entry as text and displays
+ * it in a read-only text field.
+ * <p>
+ * You can use the DataViewerContext class for creating custom log
+ * methods around logCustomContext for sending custom text data.
+ * <p>
+ * Note: This class is not guaranteed to be threadsafe.
+ */
+public class DataViewerContext extends TextContext {
+	/**
+	 * Creates and initializes a DataViewerContext instance.
+	 */
+	public DataViewerContext() {
 		super(ViewerId.Data);
 	}
 }
--- src/main/java/com/gurock/smartinspect/ErrorEvent.java
+++ src/main/java/com/gurock/smartinspect/ErrorEvent.java
@@ -1,46 +1,39 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
-// <summary>
-//   This class is used by the SmartInspectListener.onError event of the
-//   SmartInspect class and the ProtocolListener.onError event of the
-//   Protocol class.
-// </summary>
-// <remarks>
-//   It has only one public class member named getException. This member
-//   is a method, which just returns the occurred exception.
-// </remarks>
-// <threadsafety>
-//   This class is full threadsafe.
-// </threadsafety>
-
-public final class ErrorEvent extends java.util.EventObject
-{
+/**
+ * This class is used by the SmartInspectListener.onError event of the
+ * SmartInspect class and the ProtocolListener.onError event of the
+ * Protocol class.
+ *
+ * <p>It has only one public class member named getException. This member
+ * is a method, which just returns the occurred exception.</p>
+ *
+ * <p>This class is fully thread-safe.</p>
+ */
+public final class ErrorEvent extends java.util.EventObject {
 	private Exception fException;
 
-	// <summary>
-	//   Creates and initializes an ErrorEvent instance.
-	// </summary>
-	// <param name="source">The object which fired the event.</param>
-	// <param name="e">The occurred exception.</param>
-
-	public ErrorEvent(Object source, Exception e)
-	{
+	/**
+	 * Creates and initializes an ErrorEvent instance.
+	 *
+	 * @param source The object which fired the event.
+	 * @param e      The occurred exception.
+	 */
+	public ErrorEvent(Object source, Exception e) {
 		super(source);
 		this.fException = e;
 	}
 
-	// <summary>
-	//   Returns the occurred exception.
-	// </summary>
-	// <returns>The occurred exception.</returns>
-
-	public Exception getException()
-	{
+	/**
+	 * Returns the occurred exception.
+	 *
+	 * @return The occurred exception.
+	 */
+	public Exception getException() {
 		return this.fException;
 	}
 }
-

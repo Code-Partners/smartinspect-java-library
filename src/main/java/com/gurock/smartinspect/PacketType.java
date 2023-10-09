7d43f8fed57bbde37468c61f1d6d39e49eda8704
Sergey Zhukov <sergey@code-partners.com>
2023-10-09 23:34:07 +0400

    docs: XMLDoc to Javadoc in 5 more files

--- src/main/java/com/gurock/smartinspect/PacketType.java
+++ src/main/java/com/gurock/smartinspect/PacketType.java
@@ -1,68 +1,54 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
-// <summary>
-//   Represents the type of a packet. In the SmartInspect concept,
-//   there are multiple packet types each serving a special purpose.
-//   For a good starting point on packets, please have a look at the
-//   documentation of the Packet class.
-// </summary>
-// <threadsafety>
-//   This class is fully threadsafe.
-// </threadsafety>
+/**
+ * Represents the type of packet. In the SmartInspect concept,
+ * there are multiple packet types each serving a special purpose.
+ * For a good starting point on packets, please have a look at the
+ * documentation of the Packet class.
+ *
+ * <p>This class is fully threadsafe
+ */
+public final class PacketType extends Enum {
+	/**
+	 * Identifies a packet as Log Entry. Please have a look at the
+	 * documentation of the LogEntry class for information about
+	 * this packet type.
+	 */
+	public static final PacketType LogEntry = new PacketType(4, "LogEntry");
 
-public final class PacketType extends Enum
-{
-	// <summary>
-	//   Identifies a packet as Log Entry. Please have a look at the
-	//   documentation of the LogEntry class for information about
-	//   this packet type.
-	// </summary>
+	/**
+	 * Identifies a packet as Control Command. Please have a look
+	 * at the documentation of the ControlCommand class for more
+	 * information about this packet type.
+	 */
+	public static final PacketType ControlCommand = new PacketType(1, "ControlCommand");
 
-	public static final
-		PacketType LogEntry = new PacketType(4, "LogEntry");
+	/**
+	 * Identifies a packet as Watch. Please have a look at the
+	 * documentation of the Watch class for information about
+	 * this packet type.
+	 */
+	public static final PacketType Watch = new PacketType(5, "Watch");
 
-	// <summary>
-	//   Identifies a packet as Control Command. Please have a look
-	//   at the documentation of the ControlCommand class for more
-	//   information about this packet type.
-	// </summary>
+	/**
+	 * Identifies a packet as Process Flow entry. Please have a
+	 * look at the documentation of the ProcessFlow class for
+	 * information about this packet type.
+	 */
+	public static final PacketType ProcessFlow = new PacketType(6, "ProcessFlow");
 
-	public static final
-		PacketType ControlCommand = new PacketType(1, "ControlCommand");
+	/**
+	 * Identifies a packet as Log Header. Please have a look at the
+	 * documentation of the LogHeader class for information about
+	 * this packet type.
+	 */
+	public static final PacketType LogHeader = new PacketType(7, "LogHeader");
 
-	// <summary>
-	//   Identifies a packet as Watch. Please have a look at the
-	//   documentation of the Watch class for information about
-	//   this packet type.
-	// </summary>
-
-	public static final
-		PacketType Watch = new PacketType(5, "Watch");
-
-	// <summary>
-	//   Identifies a packet as Process Flow entry. Please have a
-	//   look at the documentation of the ProcessFlow class for
-	//   information about this packet type.
-	// </summary>
-
-	public static final
-		PacketType ProcessFlow = new PacketType(6, "ProcessFlow");
-
-	// <summary>
-	//   Identifies a packet as Log Header. Please have a look at the
-	//   documentation of the LogHeader class for information about
-	//   this packet type.
-	// </summary>
-
-	public static final
-		PacketType LogHeader = new PacketType(7, "LogHeader");
-
-	private PacketType(int value, String name)
-	{
+	private PacketType(int value, String name) {
 		super(value, name);
 	}
 }

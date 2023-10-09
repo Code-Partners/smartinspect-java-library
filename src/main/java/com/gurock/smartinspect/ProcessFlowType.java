64145a34c8bb063ffbbf9b4850f476fb412747e1
Sergey Zhukov <sergey@code-partners.com>
2023-10-10 00:59:48 +0400

    docs: fixes in `ProcessFlowType`

--- src/main/java/com/gurock/smartinspect/ProcessFlowType.java
+++ src/main/java/com/gurock/smartinspect/ProcessFlowType.java
@@ -1,59 +1,56 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
+
 /**
- * Represents the type of a ProcessFlow packet. The type of a Process
- * Flow entry specifies the way the Console interprets this packet. For example, 
+ * Represents the type of ProcessFlow packet. The type of a Process
+ * Flow entry specifies the way the Console interprets this packet. For example,
  * if a Process Flow entry has a type of
  * ProcessFlowType.EnterThread, the Console interprets this packet as
  * information about a new thread of your application.
  * <p>
  * This class is fully threadsafe.
  */
+public final class ProcessFlowType extends Enum {
+	/**
+	 * Instructs the Console to enter a new method.
+	 */
+	public static final ProcessFlowType EnterMethod =
+			new ProcessFlowType(0, "EnterMethod");
 
-public final class ProcessFlowType extends Enum
-{
-/**
- * Instructs the Console to enter a new method.
- */
+	/**
+	 * Instructs the Console to leave a method.
+	 */
+	public static final ProcessFlowType LeaveMethod =
+			new ProcessFlowType(1, "LeaveMethod");
 
-	public static final ProcessFlowType EnterMethod = 
-		new ProcessFlowType(0, "EnterMethod");
-    /**
-     * Instructs the Console to leave a method.
-     */
+	/**
+	 * Instructs the Console to enter a new thread.
+	 */
+	public static final ProcessFlowType EnterThread =
+			new ProcessFlowType(2, "EnterThread");
 
-	public static final ProcessFlowType LeaveMethod = 
-		new ProcessFlowType(1, "LeaveMethod");
-/**
- * Instructs the Console to enter a new thread.
- */
+	/**
+	 * Instructs the Console to leave a thread.
+	 */
+	public static final ProcessFlowType LeaveThread =
+			new ProcessFlowType(3, "LeaveThread");
 
-	public static final ProcessFlowType EnterThread = 
-		new ProcessFlowType(2, "EnterThread");
-    /**
-    * Instructs the Console to leave a thread.
-    */
+	/**
+	 * Instructs the Console to enter a new process.
+	 */
+	public static final ProcessFlowType EnterProcess =
+			new ProcessFlowType(4, "EnterProcess");
 
-	public static final ProcessFlowType LeaveThread = 
-		new ProcessFlowType(3, "LeaveThread");
-/**
- * Instructs the Console to enter a new process.
- */
+	/**
+	 * Instructs the Console to leave a process.
+	 */
+	public static final ProcessFlowType LeaveProcess =
+			new ProcessFlowType(5, "LeaveProcess");
 
-	public static final ProcessFlowType EnterProcess = 
-		new ProcessFlowType(4, "EnterProcess");
-    /**
-     * Instructs the Console to leave a process.
-     */
-
-	public static final ProcessFlowType LeaveProcess = 
-		new ProcessFlowType(5, "LeaveProcess");
-
-	private ProcessFlowType(int value, String name)
-	{
+	private ProcessFlowType(int value, String name) {
 		super(value, name);
 	}
 }

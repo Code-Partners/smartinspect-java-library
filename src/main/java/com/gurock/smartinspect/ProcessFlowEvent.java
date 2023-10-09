bda4d461a229c0747efbb511f679380ba5c1587b
Sergey Zhukov <sergey@code-partners.com>
2023-10-10 00:37:44 +0400

    docs: XMLDoc to Javadoc in 5 more files

--- src/main/java/com/gurock/smartinspect/ProcessFlowEvent.java
+++ src/main/java/com/gurock/smartinspect/ProcessFlowEvent.java
@@ -1,48 +1,34 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
-// <summary>
-//   This class is used by the SmartInspectListener.onProcessFlow event
-//   of the SmartInspect class.
-// </summary>
-// <remarks>
-//   It has only one public class member named getProcessFlow. This
-//   member is a method, which just returns the sent packet.
-// </remarks>
-// <threadsafety>
-//   This class is fully threadsafe.
-// </threadsafety>
-
-public final class ProcessFlowEvent extends java.util.EventObject
-{
+/**
+ * This class is used by the SmartInspectListener.onProcessFlow event
+ * of the SmartInspect class. It has only one public class member named getProcessFlow. This
+ * member is a method, which just returns the sent packet. This class is fully threadsafe.
+ */
+public final class ProcessFlowEvent extends java.util.EventObject {
 	private ProcessFlow fProcessFlow;
 
-	// <summary>
-	//   Creates and initializes a ProcessFlowEvent instance.
-	// </summary>
-	// <param name="source">The object which fired the event.</param>
-	// <param name="processFlow">
-	//   The ProcessFlow packet which has just been sent.
-	// </param>
-
-	public ProcessFlowEvent(Object source, ProcessFlow processFlow)
-	{
+	/**
+	 * Creates and initializes a ProcessFlowEvent instance.
+	 *
+	 * @param source      The object which fired the event
+	 * @param processFlow The ProcessFlow packet which has just been sent
+	 */
+	public ProcessFlowEvent(Object source, ProcessFlow processFlow) {
 		super(source);
 		this.fProcessFlow = processFlow;
 	}
 
-	// <summary>
-	//   Returns the ProcessFlow packet, which has just been sent.
-	// </summary>
-	// <returns>
-	//   The ProcessFlow packet which has just been sent.
-	// </returns>
-
-	public ProcessFlow getProcessFlow()
-	{
+	/**
+	 * Returns the ProcessFlow packet, which has just been sent.
+	 *
+	 * @return The ProcessFlow packet which has just been sent
+	 */
+	public ProcessFlow getProcessFlow() {
 		return this.fProcessFlow;
 	}
 }
--- src/main/java/com/gurock/smartinspect/ProcessFlowType.java
+++ src/main/java/com/gurock/smartinspect/ProcessFlowType.java
@@ -3,60 +3,51 @@
 //
 
 package com.gurock.smartinspect;
-
-// <summary>
-//   Represents the type of a ProcessFlow packet. The type of a Process
-//   Flow entry specifies the way the Console interprets this packet.
-// </summary>
-// <remarks>
-//  For example, if a Process Flow entry has a type of
-//  ProcessFlowType.EnterThread, the Console interprets this packet as
-//  information about a new thread of your application.
-// </remarks>
-// <threadsafety>
-//   This class is fully threadsafe.
-// </threadsafety>
+/**
+ * Represents the type of a ProcessFlow packet. The type of a Process
+ * Flow entry specifies the way the Console interprets this packet. For example, 
+ * if a Process Flow entry has a type of
+ * ProcessFlowType.EnterThread, the Console interprets this packet as
+ * information about a new thread of your application.
+ * <p>
+ * This class is fully threadsafe.
+ */
 
 public final class ProcessFlowType extends Enum
 {
-	// <summary>
-	//   Instructs the Console to enter a new method.
-	// </summary>
+/**
+ * Instructs the Console to enter a new method.
+ */
 
 	public static final ProcessFlowType EnterMethod = 
 		new ProcessFlowType(0, "EnterMethod");
-
-	// <summary>
-	//   Instructs the Console to leave a method.
-	// </summary>
+    /**
+     * Instructs the Console to leave a method.
+     */
 
 	public static final ProcessFlowType LeaveMethod = 
 		new ProcessFlowType(1, "LeaveMethod");
-
-	// <summary>
-	//   Instructs the Console to enter a new thread.
-	// </summary>
+/**
+ * Instructs the Console to enter a new thread.
+ */
 
 	public static final ProcessFlowType EnterThread = 
 		new ProcessFlowType(2, "EnterThread");
-
-	// <summary>
-	//   Instructs the Console to leave a thread.
-	// </summary>
+    /**
+    * Instructs the Console to leave a thread.
+    */
 
 	public static final ProcessFlowType LeaveThread = 
 		new ProcessFlowType(3, "LeaveThread");
-
-	// <summary>
-	//   Instructs the Console to enter a new process.
-	// </summary>
+/**
+ * Instructs the Console to enter a new process.
+ */
 
 	public static final ProcessFlowType EnterProcess = 
 		new ProcessFlowType(4, "EnterProcess");
-
-	// <summary>
-	//   Instructs the Console to leave a process.
-	// </summary>
+    /**
+     * Instructs the Console to leave a process.
+     */
 
 	public static final ProcessFlowType LeaveProcess = 
 		new ProcessFlowType(5, "LeaveProcess");

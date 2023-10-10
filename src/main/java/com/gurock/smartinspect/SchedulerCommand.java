a0ec832ef4303a9deacc94ee150adfa12d198daa
Sergey Zhukov <sergey@code-partners.com>
2023-10-10 23:51:56 +0400

    docs: XMLDoc to Javadoc in 5 more files

--- src/main/java/com/gurock/smartinspect/SchedulerCommand.java
+++ src/main/java/com/gurock/smartinspect/SchedulerCommand.java
@@ -1,108 +1,88 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
-// <summary>
-//   Represents a scheduler command as used by the Scheduler class
-//   and the asynchronous protocol mode.
-// </summary>
-// <remarks>
-//   This class is used by the Scheduler class to enqueue protocol
-//   operations for later execution when operating in asynchronous
-//   mode. For detailed information about the asynchronous protocol 
-//   mode, please refer to Protocol.isValidOption.
-// </remarks>
-// <threadsafety>
-//   This class is not guaranteed to be threadsafe.
-// </threadsafety>
-
-public class SchedulerCommand
-{
+/**
+ * Represents a scheduler command as used by the Scheduler class
+ * and the asynchronous protocol mode.
+ * <p>
+ * This class is used by the Scheduler class to enqueue protocol
+ * operations for later execution when operating in asynchronous
+ * mode. For detailed information about the asynchronous protocol
+ * mode, please refer to Protocol.isValidOption.
+ * </p>
+ * <p>
+ * This class is not guaranteed to be threadsafe.
+ * </p>
+ */
+public class SchedulerCommand {
 	private SchedulerAction fAction = SchedulerAction.Connect;
 	private Object fState;
-	
-	// <summary>
-	//   Returns the scheduler action to execute. Please refer
-	//   to the documentation of the SchedulerAction class for more
-	//   information about possible values.
-	// </summary>
-	// <returns>The scheduler action to execute.</returns>
-	
-	public SchedulerAction getAction()
-	{
+
+	/**
+	 * Returns the scheduler action to execute. Please refer
+	 * to the documentation of the SchedulerAction class for more
+	 * information about possible values.
+	 *
+	 * @return The scheduler action to execute
+	 */
+	public SchedulerAction getAction() {
 		return this.fAction;
 	}
-	
-	// <summary>
-	//   Sets the scheduler action to execute. Please refer to
-	//   the documentation of the SchedulerAction class for more
-	//   information about possible values.
-	// </summary>
-	// <param name="action">
-	//   The new scheduler action. Null references are ignored.
-	// </param>
 
-	public void setAction(SchedulerAction action)
-	{
-		if (action != null)
-		{
+	/**
+	 * Sets the scheduler action to execute. Please refer to
+	 * the documentation of the SchedulerAction class for more
+	 * information about possible values.
+	 *
+	 * @param action The new scheduler action. Null references are ignored
+	 */
+	public void setAction(SchedulerAction action) {
+		if (action != null) {
 			this.fAction = action;
 		}
 	}
 
-	// <summary>
-	//   Returns the optional scheduler command state object which
-	//   provides additional information about the scheduler command.
-	//   This method can return null.
-	// </summary>
-	// <returns>
-	//   The optional scheduler command state object. Can be null.
-	// </returns>
-	
-	public Object getState()
-	{
+	/**
+	 * Returns the optional scheduler command state object which
+	 * provides additional information about the scheduler command.
+	 * This method can return null.
+	 *
+	 * @return The optional scheduler command state object, can be null
+	 */
+	public Object getState() {
 		return this.fState;
 	}
-	
-	// <summary>
-	//   Sets the optional scheduler command state object which
-	//   provides additional information about the scheduler command.
-	// </summary>
-	// <param name="action">
-	//   The new scheduler command state object. Can ben null.
-	// </param>
 
-	public void setState(Object state)
-	{
+	/**
+	 * Sets the optional scheduler command state object which
+	 * provides additional information about the scheduler command.
+	 *
+	 * @param state The new scheduler command state object. Can be null
+	 */
+	public void setState(Object state) {
 		this.fState = state;
 	}
-	
-	// <summary>
-	//   Calculates and returns the total memory size occupied by
-	//   this scheduler command.
-	// </summary>
-	// <remarks>
-	//   This method returns the total occupied memory size of this
-	//   scheduler command. This functionality is used by the
-	//   <link Protocol.isValidOption, asynchronous protocol mode> to
-	//   track the total size of scheduler commands.
-	// </remarks>
-	
-	public int getSize()
-	{
-		if (this.fAction != SchedulerAction.WritePacket)
-		{
+
+	/**
+	 * Calculates and returns the total memory size occupied by
+	 * this scheduler command.
+	 * <p>
+	 * This method returns the total occupied memory size of this
+	 * scheduler command. This functionality is used by the
+	 * asynchronous protocol mode to track the total size of
+	 * scheduler commands.
+	 */
+	public int getSize() {
+		if (this.fAction != SchedulerAction.WritePacket) {
 			return 0;
 		}
-		
-		if (this.fState != null)
-		{
+
+		if (this.fState != null) {
 			return ((Packet) this.fState).getSize();
-		}
-		else 
-		{
+		} else {
 			return 0;
 		}
 	}

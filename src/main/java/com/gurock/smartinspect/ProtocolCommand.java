26590dba5e9c76ebaf7779f17b5605c70270ca02
Sergey Zhukov <sergey@code-partners.com>
2023-10-10 23:30:50 +0400

    docs: XMLDoc to Javadoc in 5 more files

--- src/main/java/com/gurock/smartinspect/ProtocolCommand.java
+++ src/main/java/com/gurock/smartinspect/ProtocolCommand.java
@@ -1,62 +1,53 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
-// <summary>
-//   Represents a custom protocol action command as used by the
-//   Protocol.dispatch method.
-// </summary>
-// <remarks>
-//   This class is used by custom protocol actions. For detailed information
-//   about custom protocol actions, please refer to the Protocol.dispatch
-//   and SmartInspect.dispatch methods.
-// </remarks>
-// <threadsafety>
-//   The public members of this class are threadsafe.
-// </threadsafety>
+/**
+ * Represents a custom protocol action command as used by the
+ * Protocol.dispatch method.
+ * <p>
+ * This class is used by custom protocol actions. For detailed information
+ * about custom protocol actions, please refer to the Protocol.dispatch
+ * and SmartInspect.dispatch methods.
+ * <p>
+ * The public members of this class are threadsafe.
+ */
 
-public class ProtocolCommand 
-{
+public class ProtocolCommand {
 	private int fAction;
 	private Object fState;
 
-	// <summary>
-	//   Creates and initializes a new ProtocolCommand instance.
-	// </summary>
-	// <param name="action">The custom protocol action to execute.</param>
-	// <param name="state">
-	//   Optional object which provides additional information about the
-	//   custom protocol action.
-	// </param>
-	
-	public ProtocolCommand(int action, Object state)
-	{
+	/**
+	 * Creates and initializes a new ProtocolCommand instance.
+	 *
+	 * @param action The custom protocol action to execute
+	 * @param state  Optional object which provides additional information about the custom protocol action
+	 */
+	public ProtocolCommand(int action, Object state) {
 		this.fAction = action;
 		this.fState = state;
 	}
-	
-	// <summary>
-	//   Returns the custom protocol action to execute. The return value
-	//   of this method is protocol specific.
-	// </summary>
-	// <returns>The custom protocol action to execute.</returns>
-	
-	public int getAction()
-	{
+
+	/**
+	 * Returns the custom protocol action to execute. The return value
+	 * of this method is protocol specific.
+	 *
+	 * @return The custom protocol action to execute
+	 */
+	public int getAction() {
 		return this.fAction;
 	}
-	
-	// <summary>
-	//   Returns the optional protocol command object which provides
-	//   additional information about the custom protocol action. This
-	//   method can return null.
-	// </summary>
-	// <returns>The optional protocol command object.</returns>
-	
-	public Object getState()
-	{
+
+	/**
+	 * Returns the optional protocol command object which provides
+	 * additional information about the custom protocol action. This
+	 * method can return null.
+	 *
+	 * @return The optional protocol command object
+	 */
+	public Object getState() {
 		return this.fState;
 	}
 }
--- src/main/java/com/gurock/smartinspect/ProtocolException.java
+++ src/main/java/com/gurock/smartinspect/ProtocolException.java
@@ -1,135 +1,116 @@
-//
-// <!-- Copyright (C) Code Partners Pty. Ltd. All rights reserved. -->
-//
+/**
+ * Copyright (C) Code Partners Pty. Ltd. All rights reserved.
+ */
 
 package com.gurock.smartinspect;
 
-// <summary>
-//   Used to report any errors concerning the protocol classes.
-// </summary>
-// <remarks>
-//   This exception can be thrown by several Protocol methods
-//   like the Protocol.connect, Protocol.disconnect or
-//   Protocol.writePacket methods when an error has occurred.
-//   
-//   See below for an example on how to obtain detailed information
-//   in the <link SmartInspectListener.onError, Error event> in the
-//   SmartInspect class about the protocol which caused the error.
-// </remarks>
-// <threadsafety>
-//   This class is not guaranteed to be threadsafe.
-// </threadsafety>
-// <example>
-// <code>
-// import com.gurock.smartinspect.*;
-//
-// class Listener extends SmartInspectAdapter
-// {
-// 	public void onError(ErrorEvent e)
-// 	{
-// 		System.out.println(e.getException());
-//
-// 		if (e.getException() instanceof ProtocolException)
-// 		{
-// 			ProtocolException pe = (ProtocolException) e.getException();
-//
-// 			// A ProtocolException provides additional information
-// 			// about the occurred error besides the normal exception
-// 			// message, like, for example, the name of the protocol
-// 			// which caused this error.
-//
-// 			System.out.println(pe.getProtocolName());
-// 			System.out.println(pe.getProtocolOptions());
-// 		}
-// 	}
-// }
-//
-//
-// public class ErrorHandling
-// {
-// 	public static void main(String[] args)
-// 	{
-// 		// Register our event handler for the error event.
-// 		SiAuto.si.addListener(new Listener());
-// 
-// 		try
-// 		{
-// 			// And force a connection error.
-// 			SiAuto.si.setConnections("file(filename=c:\\\\)");
-// 		}
-// 		catch (InvalidConnectionsException e)
-// 		{
-// 			// This catch block is useless. It won't be reached
-// 			// anyway, because a connection error doesn't result
-// 			// in a Java exception. The SmartInspect Java libary
-// 			// uses the Error event for this purpose.
-// 		}
-//
-// 		SiAuto.si.setEnabled(true);
-// 	}
-// }
-// </code>
-// </example>
-
-public final class ProtocolException extends SmartInspectException
-{
+/**
+ * This class is used to report any errors concerning the protocol classes.
+ * <p>
+ * This exception can be thrown by several Protocol methods
+ * like the Protocol.connect, Protocol.disconnect or
+ * Protocol.writePacket methods when an error has occurred.
+ * <p>
+ * See below for an example on how to obtain detailed information
+ * in the Error event in the SmartInspect class about the protocol which caused the error.
+ * <p>
+ * Note: This class is not guaranteed to be threadsafe.
+ * Example:
+ * <pre>
+ * import com.gurock.smartinspect.*;
+ *
+ * class Listener extends SmartInspectAdapter
+ * {
+ * 	public void onError(ErrorEvent e)
+ *    {
+ * 		System.out.println(e.getException());
+ *
+ * 		if (e.getException() instanceof ProtocolException)
+ *        {
+ * 			ProtocolException pe = (ProtocolException) e.getException();
+ *
+ * 			// A ProtocolException provides additional information
+ * 			// about the occurred error besides the normal exception
+ * 			// message, like, for example, the name of the protocol
+ * 			// which caused this error.
+ *
+ * 			System.out.println(pe.getProtocolName());
+ * 			System.out.println(pe.getProtocolOptions());
+ *        }
+ *    }
+ * }
+ *
+ * public class ErrorHandling
+ * {
+ * 	public static void main(String[] args)
+ *    {
+ * 		// Register our event handler for the error event.
+ * 		SiAuto.si.addListener(new Listener());
+ *
+ * 		try
+ *        {
+ * 			// And force a connection error.
+ * 			SiAuto.si.setConnections("file(filename=c:\\\\)");
+ *        }
+ * 		catch (InvalidConnectionsException e)
+ *        {
+ * 			// This catch block is useless. It won't be reached
+ * 			// anyway, because a connection error doesn't result
+ * 			// in a Java exception. The SmartInspect Java library
+ * 			// uses the Error event for this purpose.
+ *        }
+ *
+ * 		SiAuto.si.setEnabled(true);
+ *    }
+ * }
+ * </pre>
+ */
+public final class ProtocolException extends SmartInspectException {
 	private String fProtocolName;
 	private String fProtocolOptions;
-	
-	// <summary>
-	//   Creates and initializes a ProtocolException instance.
-	// </summary>
-	// <param name="e">
-	//   The error message which describes the exception.
-	// </param>
 
-	public ProtocolException(String e)
-	{
+	/**
+	 * Creates and initializes a ProtocolException instance.
+	 *
+	 * @param e The error message which describes the exception
+	 */
+	public ProtocolException(String e) {
 		super(e);
 	}
 
-	// <summary>
-	//   Returns the name of the protocol which caused this exception.
-	// </summary>
-	// <returns>
-	//   The name of the protocol which caused this exception.
-	// </returns>
-
-	public String getProtocolName()
-	{
+	/**
+	 * Returns the name of the protocol which caused this exception
+	 *
+	 * @return The name of the protocol which caused this exception
+	 */
+	public String getProtocolName() {
 		return this.fProtocolName;
 	}
 
-	// <summary>
-	//   Sets the name of the protocol which caused this exception.
-	// </summary>
-	// <param name="name">The name of the protocol.</param>
-
-	public void setProtocolName(String name)
-	{
+	/**
+	 * Sets the name of the protocol which caused this exception.
+	 *
+	 * @param name The name of the protocol
+	 */
+	public void setProtocolName(String name) {
 		this.fProtocolName = name;
 	}
 
-	// <summary>
-	//   Returns the options of the protocol which caused this
-	//   exception.
-	// </summary>
-	// <returns>
-	//   The options of the protocol which caused this exception.
-	// </returns>
-
-	public String getProtocolOptions()
-	{
+	/**
+	 * Returns the options of the protocol which caused this exception.
+	 *
+	 * @return The options of the protocol which caused this exception
+	 */
+	public String getProtocolOptions() {
 		return this.fProtocolOptions;
 	}
 
-	// <summary>
-	//   Sets the options of the protocol which caused this exception.
-	// </summary>
-	// <param name="options">The options of the protocol.</param>
-
-	public void setProtocolOptions(String options)
-	{
+	/**
+	 * Sets the options of the protocol which caused this exception.
+	 *
+	 * @param options The options of the protocol
+	 */
+	public void setProtocolOptions(String options) {
 		this.fProtocolOptions = options;
 	}
 }
